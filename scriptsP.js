cc1afe46d1c5558c04036d82cb594f6288adea82
LeanSalminci <126431763+LeanSalminci@users.noreply.github.com>
2023-02-27 20:12:17 -0300

    Add files via upload

---
+++ scriptsP.js
@@ -0,0 +1,36 @@
+let menuVisible = false;
+
+function mostrarOcultarMenu(){
+    if(menuVisible){
+        document.getElementById("nav").classList ="";
+        menuVisible = false;
+    }else{
+        document.getElementById("nav").classList ="responsive";
+        menuVisible = true;
+    }
+}
+
+function seleccionar(){
+    
+    document.getElementById("nav").classList = "";
+    menuVisible = false;
+}
+
+function efectoHabilidades(){
+    var habilidades = document.getElementById("habilidades");
+    var distancia_habilidades = window.innerHeight - habilidades.getBoundingClientRect().top;
+    if(distancia_habilidades >= 300){
+        let habilidades = document.getElementsByClassName("progreso");
+        habilidades[0].classList.add("javascript");
+        habilidades[1].classList.add("htmlcss");
+        habilidades[2].classList.add("photoshop");
+        habilidades[3].classList.add("Excel");
+    
+    }
+}
+
+
+
+window.onscroll = function(){
+    efectoHabilidades();
+} 
